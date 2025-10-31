9e8ffa6bdc22eaeb7c607f4fa4f8b0bdc2f367e6
lab_desktop <choi00608@gmail.com>
2025-10-31 15:52:53 +0900

    project add

--- js/components/projects.js
+++ js/components/projects.js
@@ -58,6 +58,24 @@ export const projects = `
                 </div>
             </div>
         </div>
+        <!-- Project 4 -->
+        <div class="col-md-4 mb-4">
+            <div class="card h-100">
+                <img src="assets/project/fortuneteller" class="card-img-top" alt="AI fortuneteller Project">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">AI fortuneteller Project</h5>
+                    <p class="card-text">AI-based fortune-telling service(사주)</p>
+                    <div class="mt-auto">
+                        <a id="btn" href="https://github.com/choi00608/fortune_teller" target="_blank" class="btn btn-secondary me-2">GitHub</a>
+                    </div>
+                    <div class="mt-3">
+                        <span class="badge rounded-pill bg-white me-1 border border-success" style="color: black; font-family: apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-size: 0.9em;"><img src="assets/logo/Python.svg" alt="Python logo" style="height: 1em; margin-right: 0.5em;">Python</span>
+                        <span class="badge rounded-pill bg-white me-1 border border-success" style="color: black; font-family: apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-size: 0.9em;"><img src="assets/logo/Django.svg" alt="Django logo" style="height: 1em; margin-right: 0.5em;">Django</span>
+                        <span class="badge rounded-pill bg-white me-1 border border-success" style="color: black; font-family: apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-size: 0.9em;"><img src="assets/logo/HTML5.svg" alt="HTML5 logo" style="height: 1em; margin-right: 0.5em;">Django</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 `;
